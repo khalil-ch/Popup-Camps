36a34a6844db40d16aff8c19a59b93331cf047b3
fatmalina1231 <61452307+fatmalina1231@users.noreply.github.com>
2021-04-23 17:09:20 +0200

    ..

--- logout.php
+++ logout.php
@@ -1,15 +1,6 @@
 <?php
-session_start(); 
-$_SESSION = array();
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 60*60,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-unset($_SESSION['login']);
-session_destroy(); // destroy session
-header("location:index.php"); 
+session_start();
+unset($_SESSION['userid']);
+unset($_SESSION['name']);
+header("Location:login.php");
 ?>
-
--- register.php
+++ register.php
@@ -1,183 +1,65 @@
-<?php
-include('includes/config.php');
-if(isset($_POST['submit']))
-{
-
-$file = $_FILES['image']['name'];
-$file_loc = $_FILES['image']['tmp_name'];
-$folder="images/"; 
-$new_file_name = strtolower($file);
-$final_file=str_replace(' ','-',$new_file_name);
-
-$name=$_POST['name'];
-$email=$_POST['email'];
-$password=md5($_POST['password']);
-$gender=$_POST['gender'];
-$mobileno=$_POST['mobileno'];
-$designation=$_POST['designation'];
-
-if(move_uploaded_file($file_loc,$folder.$final_file))
-	{
-		$image=$final_file;
-    }
-$notitype='Create Account';
-$reciver='Admin';
-$sender=$email;
-
-$sqlnoti="insert into notification (notiuser,notireciver,notitype) values (:notiuser,:notireciver,:notitype)";
-$querynoti = $dbh->prepare($sqlnoti);
-$querynoti-> bindParam(':notiuser', $sender, PDO::PARAM_STR);
-$querynoti-> bindParam(':notireciver',$reciver, PDO::PARAM_STR);
-$querynoti-> bindParam(':notitype', $notitype, PDO::PARAM_STR);
-$querynoti->execute();    
-    
-$sql ="INSERT INTO users(name,email, password, gender, mobile, designation, image, status) VALUES(:name, :email, :password, :gender, :mobileno, :designation, :image, 1)";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':name', $name, PDO::PARAM_STR);
-$query-> bindParam(':email', $email, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> bindParam(':gender', $gender, PDO::PARAM_STR);
-$query-> bindParam(':mobileno', $mobileno, PDO::PARAM_STR);
-$query-> bindParam(':designation', $designation, PDO::PARAM_STR);
-$query-> bindParam(':image', $image, PDO::PARAM_STR);
-$query->execute();
-$lastInsertId = $dbh->lastInsertId();
-if($lastInsertId)
-{
-echo "<script type='text/javascript'>alert('Registration Sucessfull!');</script>";
-echo "<script type='text/javascript'> document.location = 'index.php'; </script>";
-}
-else 
-{
-$error="Something went wrong. Please try again";
-}
-
-}
+<?php 
+include('class/User.php');
+$user = new User();
+$message =  $user->register();
+include('include/header.php');
 ?>
-
-<!doctype html>
-<html lang="en" class="no-js">
-
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-	
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
-	<link rel="stylesheet" href="css/bootstrap-social.css">
-	<link rel="stylesheet" href="css/bootstrap-select.css">
-	<link rel="stylesheet" href="css/fileinput.min.css">
-	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
-	<link rel="stylesheet" href="css/style.css">
-    <script type="text/javascript">
-
-	function validate()
-        {
-            var extensions = new Array("jpg","jpeg");
-            var image_file = document.regform.image.value;
-            var image_length = document.regform.image.value.length;
-            var pos = image_file.lastIndexOf('.') + 1;
-            var ext = image_file.substring(pos, image_length);
-            var final_ext = ext.toLowerCase();
-            for (i = 0; i < extensions.length; i++)
-            {
-                if(extensions[i] == final_ext)
-                {
-                return true;
-                
-                }
-            }
-            alert("Image Extension Not Valid (Use Jpg,jpeg)");
-            return false;
-        }
-        
-</script>
-</head>
-
-<body>
-	<div class="login-page bk-img">
-		<div class="form-content">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<h1 class="text-center text-bold mt-2x">Register</h1>
-                        <div class="hr-dashed"></div>
-						<div class="well row pt-2x pb-3x bk-light text-center">
-                         <form method="post" class="form-horizontal" enctype="multipart/form-data" name="regform" onSubmit="return validate();">
-                            <div class="form-group">
-                            <label class="col-sm-1 control-label">Name<span style="color:red">*</span></label>
-                            <div class="col-sm-5">
-                            <input type="text" name="name" class="form-control" required>
-                            </div>
-                            <label class="col-sm-1 control-label">Email<span style="color:red">*</span></label>
-                            <div class="col-sm-5">
-                            <input type="text" name="email" class="form-control" required>
-                            </div>
-                            </div>
-
-                            <div class="form-group">
-                            <label class="col-sm-1 control-label">Password<span style="color:red">*</span></label>
-                            <div class="col-sm-5">
-                            <input type="password" name="password" class="form-control" id="password" required >
-                            </div>
-
-                            <label class="col-sm-1 control-label">Designation<span style="color:red">*</span></label>
-                            <div class="col-sm-5">
-                            <input type="text" name="designation" class="form-control" required>
-                            </div>
-                            </div>
-
-                             <div class="form-group">
-                            <label class="col-sm-1 control-label">Gender<span style="color:red">*</span></label>
-                            <div class="col-sm-5">
-                            <select name="gender" class="form-control" required>
-                            <option value="">Select</option>
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                            </select>
-                            </div>
-
-                            <label class="col-sm-1 control-label">Phone<span style="color:red">*</span></label>
-                            <div class="col-sm-5">
-                            <input type="number" name="mobileno" class="form-control" required>
-                            </div>
-                            </div>
-
-                             <div class="form-group">
-                            <label class="col-sm-1 control-label">Avtar<span style="color:red">*</span></label>
-                            <div class="col-sm-5">
-                            <div><input type="file" name="image" class="form-control"></div>
-                            </div>
-                            </div>
-
-								<br>
-                                <button class="btn btn-primary" name="submit" type="submit">Register</button>
-                                </form>
-                                <br>
-                                <br>
-								<p>Already Have Account? <a href="index.php" >Signin</a></p>
+<title>webdamn.com : Demo User Management System with PHP & MySQL</title>
+<?php include('include/container.php');?>
+<div class="container contact">	
+	<h2>Example: User Management System with PHP & MySQL</h2>
+	<div id="signupbox" class="col-md-7">
+		<div class="panel panel-info">
+			<div class="panel-heading">
+				<div class="panel-title">Sign Up</div>				
+			</div>  
+			<div class="panel-body" >
+				<form id="signupform" class="form-horizontal" role="form" method="POST" action="">				
+					<?php if ($message != '') { ?>
+						<div id="login-alert" class="alert alert-danger col-sm-12"><?php echo $message; ?></div>                            
+					<?php } ?>	
+					<div class="form-group">
+						<label for="firstname" class="col-md-3 control-label">First Name*</label>
+						<div class="col-md-9">
+							<input type="text" class="form-control" name="firstname" placeholder="First Name" value="<?php if(!empty($_POST["firstname"])) { echo $_POST["firstname"]; } ?>" required>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="lastname" class="col-md-3 control-label">Last Name</label>
+						<div class="col-md-9">
+							<input type="text" class="form-control" name="lastname" placeholder="Last Name" value="<?php if(!empty($_POST["lastname"])) { echo $_POST["lastname"]; } ?>" >
+						</div>
+					</div>					
+					<div class="form-group">
+						<label for="email" class="col-md-3 control-label">Email*</label>
+						<div class="col-md-9">
+							<input type="email" class="form-control" name="email" placeholder="Email Address" value="<?php if(!empty($_POST["email"])) { echo $_POST["email"]; } ?>" required>
+						</div>
+					</div>					
+					<div class="form-group">
+						<label for="password" class="col-md-3 control-label">Password*</label>
+						<div class="col-md-9">
+							<input type="password" class="form-control" name="passwd" placeholder="Password" required>
+						</div>
+					</div>								
+					<div class="form-group">						                                  
+						<div class="col-md-offset-3 col-md-9">
+							<button id="btn-signup" type="submit" name="register" value="register" class="btn btn-info"><i class="icon-hand-right"></i> &nbsp Register</button>			
+						</div>
+					</div>					
+					<div class="form-group">
+						<div class="col-md-12 control">
+							<div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
+								If You've already an account! 
+							<a href="login.php">
+								Log In 
+							</a>Here
 							</div>
 						</div>
-				</div>
-			</div>
+					</div>  				
+				</form>
+			 </div>
 		</div>
-	</div>
-	
-	<!-- Loading Scripts -->
-	<script src="js/jquery.min.js"></script>
-	<script src="js/bootstrap-select.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.dataTables.min.js"></script>
-	<script src="js/dataTables.bootstrap.min.js"></script>
-	<script src="js/Chart.min.js"></script>
-	<script src="js/fileinput.js"></script>
-	<script src="js/chartData.js"></script>
-	<script src="js/main.js"></script>
-
-</body>
-</html>
+	</div>	
+</div>
+<?php include('include/footer.php');?>
