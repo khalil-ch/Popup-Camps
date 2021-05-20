565e3b233787d22502b6a199dcb3984cae99288b
fatmalina1231 <61452307+fatmalina1231@users.noreply.github.com>
2021-05-20 20:05:38 +0200

    lemme

--- back/View/supprimerCommandeItem.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../Controller/commandeItemsC.php';
-
-
-$commandeItem= new commandeItemsC();
-$commandeItem->supprimerCommandeItem($_GET['idCommande'],$_GET['idProduit']);
-header("Location:afficherCommande.php?id=".$_GET['idCommande']);
-echo "deleted sucessfuly";
-?>
--- front/View/json.php
+++
@@ -1,8 +0,0 @@
-<?php 
-   
-   if(isset($_POST)){
-       $str_json = file_get_contents('php://input');
-       echo $str_json;
-   }
-
-?>
